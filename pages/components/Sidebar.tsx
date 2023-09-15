9b61fe60eef0a19773a88026e7908cf3c4dcf39e
LamCuteVjpPro <letronglam26320003@gmail.com>
2023-09-15 14:56:47 +0700

    first commit

---
+++ pages/components/Sidebar.tsx
@@ -0,0 +1,72 @@
+import styled from "styled-components";
+import Avatar from "@mui/material/Avatar";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVerticalIcon from "@mui/icons-material/MoreVert";
+import LogoutIcon from "@mui/icons-material/Logout";
+import SearchIcon from "@mui/icons-material/Search";
+
+const StyledContainer = styled.div`
+    height: 100vh;
+    min-width: 300px;
+    max-width: 350px;
+    overflow-y: scroll;
+    border-right: 1px solid whitesmoke;
+`;
+
+const StyledHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px;
+    height: 80px;
+    border-bottom: 1px solid whitesmoke;
+    position: sticky;
+    top: 0;
+    background-color: white;
+    z-index: 1;
+
+`;
+
+const StyledSearch = styled.div``;
+
+const StyledSidebarButton = styled.button``;
+
+const StyledUserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const Sidebar = () => {
+  return (
+    <StyledContainer>
+      <StyledHeader>
+        <Tooltip title="USER EMAIL" placement="right">
+          <StyledUserAvatar />
+        </Tooltip>
+        <div>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVerticalIcon />
+          </IconButton>
+          <IconButton>
+            <LogoutIcon />
+          </IconButton>
+        </div>
+      </StyledHeader>
+
+      <StyledSearch></StyledSearch>
+
+      <StyledSidebarButton></StyledSidebarButton>
+
+      {/* List of conversations */}
+    </StyledContainer>
+  );
+};
+
+export default Sidebar;
